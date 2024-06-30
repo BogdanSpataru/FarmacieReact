385257d477f274744ece302981b57323abbafb7e
BogdanSpataru <bogdan.spataru96@gmail.com>
2024-06-30 21:04:29 +0300

    Fix favorite button status.

--- src/components/Produse/FavoriteButton.jsx
+++ src/components/Produse/FavoriteButton.jsx
@@ -2,28 +2,24 @@ import React, { useState, useEffect } from "react";
 import { FavoriteButton, FavoriteIcon } from "./Produse.styled";
 import { useContext } from "react";
 import { CartContext } from "../../store/context";
-import {
-  addToCart,
-  removeFromCart,
-  removeFromFav,
-  addToFav,
-} from "../../store/actions";
+import { removeFromFav, addToFav } from "../../store/actions";
 
-const FavoriteButtonComponent = ({ id, name, img, price, isfavorite }) => {
-  const [isFav, setIsFav] = useState(isfavorite);
-  const { state, dispatchCart } = useContext(CartContext);
+const isFavorite = (store, id) => store.favValue.some((prod) => prod.id === id);
+
+const FavoriteButtonComponent = ({ id, name, img, price }) => {
+  const { stateGlobalCart, dispatchCart } = useContext(CartContext);
+
+  let [isFav, setIsFav] = useState(isFavorite(stateGlobalCart, id));
   useEffect(() => {
-    setIsFav(isfavorite);
-  }, [isfavorite]);
+    setIsFav(isFavorite(stateGlobalCart, id));
+  }, [stateGlobalCart]);
 
   const handleFavoriteClick = () => {
-    console.log(isFav);
     if (isFav) {
       dispatchCart(removeFromFav(id));
     } else {
       dispatchCart(addToFav(id, name, img, price));
     }
-    setIsFav(!isFav);
   };
 
   return (
